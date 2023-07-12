731c5d733ceaddcaac6be45d144a8b79c1668c96
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-07-12 16:59:55 +0700

    adding useEffect

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../App.css';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 
 function About() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <nav>TESTING</nav>
-      </header> */}
-      <Navbar />
-      <h1>This is About Page.</h1>
-      <Footer />
-    </div>
-  );
+    const [getAge, setAge] = useState(0)
+    const [getCategory, setCategory] = useState("Balita")
+
+    useEffect(() => {
+        if (getAge > 5 && getAge < 13) {
+            setCategory("Anak-anak")
+        } else if (getAge > 12 && getAge < 18) {
+            setCategory("Remaja")
+        } else if (getAge > 17) {
+            setCategory("Dewasa")
+        } else {
+            setCategory("Balita")
+        }
+    }, [getAge])
+
+    return (
+        <div className="App">
+        {/* <header className="App-header">
+            <nav>TESTING</nav>
+        </header> */}
+        <Navbar />
+        <h1>This is About Page.</h1>
+        <h1>Umur saya : {getAge} Tahun</h1>
+        <h3>{getCategory}</h3>
+        <button onClick={() => setAge((prev) => prev + 1)}>Tambah</button>
+        <button onClick={() => setAge((prev) => prev - 1)}>Kurang</button>
+        <Footer />
+        </div>
+    );
 }
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import '../App.css';
 import Intro from '../components/intro';
 import Navbar from '../components/navbar';
@@ -14,6 +14,10 @@ function Home() {
   const changeNavbar = () => {
     setNavbarValue("Contact");
   }
+
+  useEffect(() => {
+    alert('Wellcome to my Website.')
+  }, [])
 
   return (
     <div className="App">
